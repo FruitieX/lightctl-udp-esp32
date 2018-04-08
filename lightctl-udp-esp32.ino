a856bbaa77b6b90b4502669b3edfcc2d3f63aa2f
Rasmus Eskola <fruitiex@gmail.com>
2018-04-08 15:43:09 +0300

    Gamma, contrast and better dither

--- lightctl-udp-esp32.ino
+++ lightctl-udp-esp32.ino
@@ -22,8 +22,8 @@ WiFiUDP Udp;
 #if CONFIG == 0
 #define ID "Fönsterbräde"
 #define NUM_STRIPS 1
-#define DATA_PINS_ {13}
-#define NUM_LEDS_ {89}
+constexpr uint16_t DATA_PINS[] = {13};
+constexpr uint16_t NUM_LEDS[] = {89};
 #define TOTAL_LEDS 89
 
 #define MAX_MA 1600 // Power limiter, consider what your power supply & wiring can handle. Remember that the ESP32+WiFi also draws power. (100-200 mA?)
@@ -31,18 +31,18 @@ WiFiUDP Udp;
 #elif CONFIG == 1
 #define ID "Skumppaflaska"
 #define NUM_STRIPS 1
-#define DATA_PINS_ {13}
-#define NUM_LEDS_ {60}
+constexpr uint16_t DATA_PINS[] = {13};
+constexpr uint16_t NUM_LEDS[] = {60};
 #define TOTAL_LEDS 60
 
-#define MAX_MA 750
+#define MAX_MA 1000
 
 #elif CONFIG == 2
 #define ID "WC väggskåp"
 
 #define NUM_STRIPS 2
-#define DATA_PINS_ {12, 13}
-#define NUM_LEDS_ {60, 73}
+constexpr uint16_t DATA_PINS[] = {12, 13};
+constexpr uint16_t NUM_LEDS[] = {60, 73};
 #define TOTAL_LEDS 133
 
 #define MAX_MA 1500
@@ -52,12 +52,79 @@ WiFiUDP Udp;
 // Enable debug mode
 //#define DEBUG
 
-constexpr uint16_t DATA_PINS[] = DATA_PINS_;
-constexpr uint16_t NUM_LEDS[] = NUM_LEDS_;
+uint8_t gamma_lookup_r[256];
+uint8_t dither_lookup_r[256];
+uint8_t gamma_lookup_g[256];
+uint8_t dither_lookup_g[256];
+uint8_t gamma_lookup_b[256];
+uint8_t dither_lookup_b[256];
+
+// actual gamma value is / 100
+uint8_t GAMMA_RED = 220;
+uint8_t GAMMA_GREEN = 250;
+uint8_t GAMMA_BLUE = 180;
+uint8_t CONTRAST_RED = 255;
+uint8_t CONTRAST_GREEN = 255;
+uint8_t CONTRAST_BLUE = 255;
+
+// at some point we don't need to dither anymore
+uint8_t dither_cutoff = 64;
+
+void calcGammaChannel(uint8_t gamma_lookup[], uint8_t dither_lookup[], uint8_t gamma, uint8_t contrast) {
+  uint8_t i = 0;
+
+  do {
+    double x = (double) i / 255.0;
+    double val = (double) contrast / 255.0 * pow(x, (double) gamma / 100.0);
+
+    gamma_lookup[i] = floor(val * 255.0);
+
+    double trash;
+    dither_lookup[i] = i >= dither_cutoff ? 0 : floor(modf(val * 255.0, &trash) * 255.0);
+  } while (i++ != 255);
+
+  // zero always means off
+  gamma_lookup[i] = 0;
+  dither_lookup[i] = 0;
+
+  // set any following zero values to 1; this way we get slightly
+  // less wrong color at extremely low brightness
+  for (i = 1;; i++) {
+    if (dither_lookup[i] == 0) {
+      dither_lookup[i] = 1;
+    } else {
+      break;
+    }
+  }
+
+  #ifdef DEBUG
+  Serial.println("gamma_lookup:");
+  for (i = 0; i < 60; i++) {
+    Serial.print(String(gamma_lookup[i]) + ",\t");
+  }
+  Serial.println("...");
+
+  Serial.println("dither_lookup:");
+  for (i = 0; i < 60; i++) {
+    Serial.print(String(dither_lookup[i]) + ",\t");
+  }
+  Serial.println("...");
+  #endif
+}
+
+void recalcGamma() {
+  #ifdef DEBUG
+  Serial.println("recalcGamma()" + String(GAMMA_RED) + ", " + String(GAMMA_GREEN) + ", " + String(GAMMA_BLUE));
+  #endif
+  calcGammaChannel(gamma_lookup_r, dither_lookup_r, GAMMA_RED, CONTRAST_RED);
+  calcGammaChannel(gamma_lookup_g, dither_lookup_g, GAMMA_GREEN, CONTRAST_GREEN);
+  calcGammaChannel(gamma_lookup_b, dither_lookup_b, GAMMA_BLUE, CONTRAST_BLUE);
+}
 
 CRGB leds[NUM_STRIPS][TOTAL_LEDS];
 
-uint8_t cur_payload[TOTAL_LEDS * 3 + 1]; // last uint8_t specifies amount of dither steps (0 disables dithering)
+#define PACKET_LENGTH TOTAL_LEDS * 3 + 7
+uint8_t cur_payload[PACKET_LENGTH]; // last uint8_t specifies amount of dither steps (0 disables dithering)
 
 void checkWifi() {
   if (WiFi.status() != WL_CONNECTED) {
@@ -66,25 +133,25 @@ void checkWifi() {
     WiFi.begin(SSID, PASSWORD);
 
     unsigned long timeout = millis() + 60000;
-    
+
     // Cool animation while connecting to wifi :-)
     uint16_t index = 0;
     CHSV loading_color = CHSV(0, 255, 255);
     while(WiFi.status() != WL_CONNECTED) {
       delay(20);
-  
+
       loading_color.hue++;
 
       for (uint8_t strip = 0; strip < NUM_STRIPS; strip++) {
         leds[strip][index % (NUM_LEDS[strip])] = loading_color;
-        
+
         for (uint16_t i = 0; i < NUM_LEDS[strip]; i++) {
           leds[strip][i].fadeToBlackBy(10);
         }
       }
-      
+
       index++;
-  
+
       FastLED.show();
 
       if (millis() > timeout) break;
@@ -106,11 +173,6 @@ void sendKeepalive() {
 void WiFiEvent(WiFiEvent_t event){
   switch(event) {
   case SYSTEM_EVENT_STA_GOT_IP:
-    //When connected set 
-    //Serial.print("WiFi connected! IP address: ");
-    //Serial.println(WiFi.localIP());  
-    //initializes the UDP state
-    //This initializes the transfer buffer
     Udp.begin(WiFi.localIP(), PORT);
     connected = true;
     break;
@@ -121,7 +183,11 @@ void WiFiEvent(WiFiEvent_t event){
   }
 }
 
-void setup() { 
+void setup() {
+  #ifdef DEBUG
+  Serial.begin(115200);
+  #endif
+
   // for loop won't do here because FastLED wants compile time constants
   FastLED.addLeds<WS2812Controller800Khz, DATA_PINS[0], GRB>(leds[0], NUM_LEDS[0]);
   #if NUM_STRIPS > 1
@@ -134,25 +200,24 @@ void setup() {
   FastLED.addLeds<WS2812Controller800Khz, DATA_PINS[3], GRB>(leds[3], NUM_LEDS[3]);
   #endif
   // etc... feel free to add more if needed
-  
+
   FastLED.setMaxPowerInVoltsAndMilliamps(5, MAX_MA);
   FastLED.setDither(0);
   FastLED.show(); // leds shine white by default, turn them off after esp32 has booted
-  
+
+  recalcGamma();
   checkWifi();
 
   ArduinoOTA.setHostname(ID);
   ArduinoOTA.begin();
 }
 
-uint8_t val_lookup[256] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,2,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,7,7,7,8,8,9,9,9,10,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18,19,19,20,20,21,21,22,23,23,24,25,25,26,26,27,28,28,29,30,30,31,32,33,33,34,35,36,36,37,38,39,39,40,41,42,43,43,44,45,46,47,48,49,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,81,82,83,84,85,86,87,89,90,91,92,93,95,96,97,98,100,101,102,103,105,106,107,108,110,111,112,114,115,116,118,119,121,122,123,125,126,127,129,130,132,133,135,136,138,139,141,142,144,145,147,148,150,151,153,154,156,157,159,160,162,164,165,167,169,170,172,173,175,177,178,180,182,183,185,187,189,190,192,194,196,197,199,201,203,204,206,208,210,212,213,215,217,219,221,223,225,226,228,230,232,234,236,238,240,242,244,246,248,250,252,254};
-uint8_t dither_lookup[256] = {0,1,3,8,15,24,35,48,63,80,99,120,143,168,195,224,0,32,67,104,143,184,227,16,63,112,163,216,15,72,131,192,0,64,131,200,15,88,163,240,63,144,227,56,143,232,67,160,0,96,195,40,143,248,99,208,63,176,35,152,15,136,3,128,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 uint8_t dither_value = 0;
 
 void set_led (uint16_t offs, uint8_t r, uint8_t g, uint8_t b) {
   uint8_t strip = 0;
 
-  // Find out which strip to control and led count before said strip
+  // Find out which strip to control and led count prior to that strip
   uint16_t cnt_before = 0;
   for (uint8_t i = 0; i < NUM_STRIPS; i++) {
     strip = i;
@@ -165,8 +230,34 @@ void set_led (uint16_t offs, uint8_t r, uint8_t g, uint8_t b) {
     // Otherwise add current strip led count to cnt_before
     cnt_before += NUM_LEDS[strip];
   }
-  
+
   leds[strip][offs - cnt_before].setRGB(r, g, b);
+}
+
+uint16_t dither_table[12] = {
+  0b000000000000, //0%
+  //0b100000000000, //8% // skip over this to reduce extreme flicker
+  0b100000100000, //16%
+  0b100010001000, //25%
+  0b100100100100, //33%
+  0b110100100100, //42%
+  0b101010101010, //50%
+  0b110101010101, //58%
+  0b110110110110, //66%
+  0b111011101110, //75%
+  0b111110111110, //83%
+  0b111111111110, //92%
+  0b111111111111, //100%
+};
+
+boolean should_dither(uint8_t val, uint8_t tresh) {
+  // rounding up division: https://stackoverflow.com/a/2745086
+  uint8_t index = (tresh * 12 + 256 - 1) / 256;
+  index = index > 11 ? 11 : index;
+
+  uint16_t table_entry = dither_table[index];
+  //val = val % 12;
+  return (table_entry >> val) & 1;
 }
 
 unsigned long lastKeepalive = 0;
@@ -177,7 +268,26 @@ void loop() {
 
   int packetSize = Udp.parsePacket();
   if (packetSize) {
-    Udp.read(cur_payload, TOTAL_LEDS * 3 + 1);
+    Udp.read(cur_payload, PACKET_LENGTH);
+
+    boolean gamma_changed =
+      cur_payload[TOTAL_LEDS * 3 + 1] != GAMMA_RED ||
+      cur_payload[TOTAL_LEDS * 3 + 2] != GAMMA_GREEN ||
+      cur_payload[TOTAL_LEDS * 3 + 3] != GAMMA_BLUE ||
+      cur_payload[TOTAL_LEDS * 3 + 4] != CONTRAST_RED ||
+      cur_payload[TOTAL_LEDS * 3 + 5] != CONTRAST_GREEN ||
+      cur_payload[TOTAL_LEDS * 3 + 6] != CONTRAST_BLUE;
+
+    GAMMA_RED = cur_payload[TOTAL_LEDS * 3 + 1];
+    GAMMA_GREEN = cur_payload[TOTAL_LEDS * 3 + 2];
+    GAMMA_BLUE = cur_payload[TOTAL_LEDS * 3 + 3];
+    CONTRAST_RED = cur_payload[TOTAL_LEDS * 3 + 4];
+    CONTRAST_GREEN = cur_payload[TOTAL_LEDS * 3 + 5];
+    CONTRAST_BLUE = cur_payload[TOTAL_LEDS * 3 + 6];
+
+    if (gamma_changed) {
+      recalcGamma();
+    }
   }
 
   if (!lastKeepalive || millis() - lastKeepalive > 5000) {
@@ -185,10 +295,9 @@ void loop() {
     sendKeepalive();
   }
 
-  uint8_t dither_steps = cur_payload[TOTAL_LEDS * 3];
-  if (dither_steps != 0) {
-    dither_value = dither_value + 256 / dither_steps;
-  } else {
+  dither_value = (dither_value + 1) % 12;
+  if (!cur_payload[TOTAL_LEDS * 3 + 0]) {
+    // disable dithering if requested
     dither_value = 0;
   }
 
@@ -197,12 +306,12 @@ void loop() {
     uint8_t g = cur_payload[i * 3 + 1];
     uint8_t b = cur_payload[i * 3 + 2];
 
-    r = dither_value < dither_lookup[r] ? val_lookup[r] + 1: val_lookup[r];
-    g = dither_value < dither_lookup[g] ? val_lookup[g] + 1: val_lookup[g];
-    b = dither_value < dither_lookup[b] ? val_lookup[b] + 1: val_lookup[b];
+    uint8_t r_adjusted = should_dither(dither_value, dither_lookup_r[r]) ? gamma_lookup_r[r] + 1: gamma_lookup_r[r];
+    uint8_t g_adjusted = should_dither(dither_value, dither_lookup_g[g]) ? gamma_lookup_g[g] + 1: gamma_lookup_g[g];
+    uint8_t b_adjusted = should_dither(dither_value, dither_lookup_b[b]) ? gamma_lookup_b[b] + 1: gamma_lookup_b[b];
 
-    set_led(i, r, g, b);
+    set_led(i, r_adjusted, g_adjusted, b_adjusted);
   }
-  
+
   FastLED.show();
 }
